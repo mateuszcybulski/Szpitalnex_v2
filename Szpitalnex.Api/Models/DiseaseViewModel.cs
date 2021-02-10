890eee84373671bbd197351a627d4ece1db57138
Mateusz Cybulski <mateusz.cybulski345@gmail.com>
2021-02-10 16:13:11 +0100

    Revert "Revert "Add filled ViewModels""
    This reverts commit 4c56a2d1f82c5e8e2241b074bf95f8d53560bf16.

---
+++ Szpitalnex.Api/Models/DiseaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Szpitalnex_v0.ViewModels
+{
+    public class DiseaseViewModel
+    {
+        public int Id { get; set; }
+        public string Chapter { get; set; }
+        public string Chapter_Desc { get; set; }
+        public string Code { get; set; }
+        public string Group_Desc { get; set; }
+        public string Group_Code { get; set; }
+        public string Subgroup { get; set; }
+        public string Subgroup_Code { get; set; }
+        public string Subgroup2 { get; set; }
+        public string Subgroup2_Code { get; set; }
+        public string ICD_3_Code { get; set; }
+        public string ICD_3_Code_Desc { get; set; }
+        public string ICD_10_Code { get; set; }
+        public string ICD_10_Code_Desc { get; set; }
+    }
+}
