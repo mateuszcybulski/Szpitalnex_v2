63f61e4b48422764bfa283440f5c2dd63cad3ff0
Mateusz Cybulski <mateusz.cybulski345@gmail.com>
2021-02-10 15:56:50 +0100

    Add filled ViewModels

---
+++ Szpitalnex.Api/Models/DoctorAvailabilityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Szpitalnex_v0.ViewModels
+{
+    public class DoctorAvailabilityViewModel
+    {
+        public DateTime DateAvailabilityDoctor { get; set; }
+        public int Id { get; set; }
+        public DoctorViewModel Doctor { get; set; }
+        public bool Actual { get; set; }
+        public int Room { get; set; }
+    }
+}
---
+++ Szpitalnex.Api/Models/SpecializationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Szpitalnex_v0.ViewModels
+{
+    public class SpecializationViewModel
+    {
+        public int Id { get; set; }
+        public string NameSpecialization { get; set; }
+        public IEnumerable<ReferralViewModel> Referrals { get; set; }
+    }
+}
